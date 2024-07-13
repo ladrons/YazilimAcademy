52ab791a0bb19486e5114f9beaa415919d674264
Burak Çevik <burak_cevik76@hotmail.com.tr>
2024-07-13 17:26:01 +0300

    Şifrelerin kaydedildiği liste yapısı güncellendi.(HashSet), BlazoredToast kütüphanesi eklendi.

--- GeneratePassword/Program.cs
+++ GeneratePassword/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using GeneratePassword;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredToast();
 
 await builder.Build().RunAsync();
