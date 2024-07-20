9624f52f612b3997f638d4c40f42a8f084699573
Burak Çevik <burak_cevik76@hotmail.com.tr>
2024-07-20 19:15:14 +0300

    July 20 lecture notes added.

---
+++ GiftSuggestionApp/Enums/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace GiftSuggestionApp.Enums
+{
+    public enum Gender
+    {
+        Female = 1,
+        Male = 2,
+    }
+}
---
+++ GiftSuggestionApp/Enums/RelationshipType.cs
@@ -0,0 +1,15 @@
+﻿namespace GiftSuggestionApp.Enums
+{
+    public enum RelationshipType
+    {
+        Friend = 1, // Arkadaş
+        Relative = 2, // Akraba
+        Parent = 3, // Ebeveyn
+        Brother = 4, // Erkek Kardeş
+        Sister = 5, // Kız Kardeş
+        Child = 6, // Çocuk
+        Spouse = 7, // Eş
+        Colleague = 8, // İş Arkadaşı
+        Other = 9 // Diğer
+    }
+}
